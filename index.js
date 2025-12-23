9ce4f4a4ce356ac0fd536060dfd84d2ed84713f7
Velizar N. Chandar <91207506+veizarel@users.noreply.github.com>
2025-12-23 18:02:58 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const mineflayer = require('mineflayer');
+const express = require('express');
+const app = express();
+
+// 1. Създаваме малък уеб сървър, за да не заспива Replit
+app.get('/', (req, res) => {
+  res.send('Ботът е зареден и работи!');
+});
+
+app.listen(3000, () => {
+  console.log('Уеб сървърът е онлайн на порт 3000');
+});
+
+// 2. Конфигурация на Minecraft бота
+const botArgs = {
+  host: 'primerno6pek.mcsh.io', // Сложи IP-то тук
+  port: 25565,                  // Сложи порта тук
+  username: 'DrujServeraBuden',
+  version: '1.21.10'             // Сложи твоята версия
+};
+
+function createBot() {
+  const bot = mineflayer.createBot(botArgs);
+
+  bot.on('spawn', () => {
+    console.log('Ботът влезе в сървъра!');
+    setTimeout(() => {
+      // Тук сложи твоята парола
+      bot.chat('/login budensum1'); 
+    }, 3000);
+  });
+
+  bot.on('end', () => {
+    console.log('Ботът излезе. Рестартиране...');
+    setTimeout(createBot, 10000);
+  });
+
+  bot.on('error', (err) => console.log('Грешка:', err));
+}
+
+createBot();
